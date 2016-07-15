ef438a28a6a015e47deaa1927914daa53f4169c0
fbaffie <fbaffie@gmail.com>
2016-07-15 11:18:05 +0200

    added skeleton of UI

--- ui.R
+++ ui.R
@@ -1,16 +1,28 @@
 # This is the user-interface definition of a Shiny web application.
 
-ui <- fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      
-    mapModuleUI("map1")
-      
-    ),
-    mainPanel(
-
-      forecast_plot_modUI("forecast_plot")
-    
-    )
-  )
+ui <- navbarPage("Flomvarsling",  # cut off:  id = "nav",
+                 
+                 tabPanel("About", icon = icon("info"),
+                          
+                          fluidRow(
+                            column(8, wellPanel(
+                              HTML('
+<p style="margin-left:1em; style="text-align:justify"> Blabla... flood data in Norway </p>')
+                              )    
+                            ))
+                          
+                 ),
+                 tabPanel("Forecast HBV_2014", icon = icon("line-chart"),
+                          
+                          forecast_plot_modUI("forecast_plot"),
+                          mapModuleUI("map1")
+                 ),
+                 navbarMenu("Mapping tools", icon = icon("globe"),
+                            tabPanel("Map1"),
+                            tabPanel("Map2")
+                 )
 )
+      
+
+
+    
