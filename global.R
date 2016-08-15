9b21b9199c7fffc30af66bd66a2172b50cfdf387
fbaffie <fbaffie@gmail.com>
2016-08-15 11:03:41 +0200

    update to checkout

--- global.R
+++ global.R
@@ -25,14 +25,14 @@ ipak(packages)
 
 # Loading NVEDATA to make sure I can update the data
 
-# if (!'devtools' %in% installed.packages()) {install.packages('devtools')}
-# library(devtools)
-# remove.packages('NVEDATA')  # Added this for the moment as the NVEDATA package may have been updated in the meantime
-# # To tidy up later by tracking the version number rather than uninstalling arbitrarily!
-# install_github("fbaffie/NVEDATA", ref = "florian")
+if (!'devtools' %in% installed.packages()) {install.packages('devtools')}
+library(devtools)
+remove.packages('NVEDATA')  # Added this for the moment as the NVEDATA package may have been updated in the meantime
+# To tidy up later by tracking the version number rather than uninstalling arbitrarily!
+install_github("fbaffie/NVEDATA", ref = "florian")
 
-# library(NVEDATA)
-# load_flood_data()
+library(NVEDATA)
+load_flood_data()
 
 ############################################################
 
