132b3f74dcccc59a9db9f9df7fea69ad439d257c
fbaffie <fbaffie@gmail.com>
2016-09-05 15:11:49 +0200

    adaptable plot size for polygon tab, same default plotted variables also

--- global.R
+++ global.R
@@ -1,22 +1,22 @@
-# # Installing and loading required packages (https://gist.github.com/stevenworthington/3178163)
-# ipak <- function(pkg){
-#   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-#   if (length(new.pkg)) 
-#     install.packages(new.pkg, dependencies = TRUE)
-#   sapply(pkg, library, character.only = TRUE)
-# }
-# 
-# 
-# # Special case for leaflet which comes from a fork of Rcura on my repo
-# packages <- c("leaflet")
-# if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
-#   install.packages('devtools')
-#   library(devtools)
-#   install_github("fbaffie/leaflet")
-# }
-# 
-# packages <- c("curl", "shiny", "magrittr", "sp", "plotly", "dplyr", "ggplot2", "lubridate", "leaflet", "shinyBS")
-# ipak(packages)
+# Installing and loading required packages (https://gist.github.com/stevenworthington/3178163)
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, library, character.only = TRUE)
+}
+
+
+# Special case for leaflet which comes from a fork of Rcura on my repo
+packages <- c("leaflet")
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages('devtools')
+  library(devtools)
+  install_github("fbaffie/leaflet")
+}
+
+packages <- c("curl", "shiny", "magrittr", "sp", "plotly", "dplyr", "ggplot2", "lubridate", "leaflet", "shinyBS")
+ipak(packages)
 # sp: For the point.in.polygon function
 
 ############################################################
