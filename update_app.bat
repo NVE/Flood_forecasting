14cd94f4c94862117772c539eac42dd403471aa2
fbaffie <fbaffie@gmail.com>
2016-09-30 12:00:00 +0200

    updating data files

--- update_app.bat
+++ update_app.bat
@@ -1,2 +1,6 @@
 @echo off
+cd C:\Users\flbk\Mine dokumenter\Github\Flood_forecasting
+git checkout demo
 "C:\Program Files\R\R-3.3.1\bin\x64\R.exe" CMD BATCH D:\Github\Flood_forecasting\update_app.R
+git commit -am "updating data files"
+git push origin
