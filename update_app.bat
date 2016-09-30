2c5a262311845199754d747a285a1335b540514b
fbaffie <fbaffie@gmail.com>
2016-09-30 20:18:05 +0200

    updating data files

--- update_app.bat
+++ update_app.bat
@@ -1,2 +1,36 @@
 @echo off
-"C:\Program Files\R\R-3.3.1\bin\x64\R.exe" CMD BATCH D:\Github\Flood_forecasting\update_app.R
+
+cd C:\Users\flbk\Mine dokumenter\Github\Flood_forecasting
+git checkout demo
+
+"C:\Users\flbk\Documents\R\R-3.3.0\bin\i386\R.exe" CMD BATCH C:\Users\flbk\Documents\GitHub\Flood_forecasting\update_app.R
+timeout 180
+
+REM git rm --cached *.RData 
+REM timeout 2
+
+REM git add --force HBV_2014.RData
+REM git add --force HBV_2016.RData
+REM git add --force HBV_past_year.RData
+REM git add --force flomtabell.RData
+REM git add --force DDD.RData
+REM timeout 2
+
+git status
+timeout 2
+
+git add -A
+git status
+timeout 2
+
+git commit -m "updating data files"
+timeout 2
+
+git push origin
+timeout 2
+
+git revert HEAD
+timeout 2
+
+git status
+timeout 2
