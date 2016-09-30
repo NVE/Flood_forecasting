502a73b76bc48a1a2a88a1dde1dd300d6537c651
fbaffie <fbaffie@gmail.com>
2016-09-30 13:07:08 +0200

    update script that reload rdata into the repo

--- update_app.bat
+++ update_app.bat
@@ -1,6 +1,8 @@
 @echo off
 cd C:\Users\flbk\Mine dokumenter\Github\Flood_forecasting
 git checkout demo
+git rm --cached *.RData 
 "C:\Program Files\R\R-3.3.1\bin\x64\R.exe" CMD BATCH D:\Github\Flood_forecasting\update_app.R
+git add -f *.Rdata
 git commit -am "updating data files"
 git push origin
