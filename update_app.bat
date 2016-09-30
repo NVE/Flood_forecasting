7193e29f6b961bbfdd922649129dd8355463b39b
fbaffie <fbaffie@gmail.com>
2016-09-30 15:29:18 +0200

    updating data files

--- update_app.bat
+++ update_app.bat
@@ -1,14 +1,21 @@
 @echo off
-git checkout demo
-REM git add --force HBV_2014.RData
-git rm --cached *.RData 
-timeout 5
-REM "C:\Users\flbk\Documents\R\R-3.3.0\bin\i386\R.exe" CMD BATCH C:\Users\flbk\Documents\GitHub\Flood_forecasting\update_app.R
 cd C:\Users\flbk\Mine dokumenter\Github\Flood_forecasting
 
 
+REM "C:\Users\flbk\Documents\R\R-3.3.0\bin\i386\R.exe" CMD BATCH C:\Users\flbk\Documents\GitHub\Flood_forecasting\update_app.R
 
+git checkout demo
+git rm --cached *.RData 
+timeout 5
+
+git add --force HBV_2014.RData
+timeout 5
+
+git status
+timeout 5
 
 git add -A
 timeout 5
+
 git status
+timeout 5
