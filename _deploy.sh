fce465b9365df87636226a221aec5a5eebf1c1b1
Florian Kobierska Baffie <fbaffie@gmail.com>
2016-10-11 19:27:32 +0200

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -14,5 +14,5 @@ git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git b
 cd book-output
 cp -r ../_book/* ./
 git add --all *
-git commit -m "Update the flood forecasting report"
+git commit -m "Update the book"
 git push origin gh-pages
