3de8c2554cd0d929ea6a717eabb0afc1540a0031
Florian Kobierska Baffie <fbaffie@gmail.com>
2016-10-12 13:59:49 +0200

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
+[ -z "${GITHUB_PAT_FLOOD}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "demo" ] && exit 0
 
 # configure your name and email if you have not done so
@@ -10,7 +10,7 @@ git config --global user.email "fbaffie@gmail.com"
 git config --global user.name "Florian Kobierska Baffie"
 
 # clone the repository to the book-output directory
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+git clone -b gh-pages https://${GITHUB_PAT_FLOOD}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
