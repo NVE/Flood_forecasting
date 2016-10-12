18bfca566c6b4097127bc3ef8de7cd2819f6a1a7
Florian Kobierska Baffie <fbaffie@gmail.com>
2016-10-12 13:12:40 +0200

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -5,10 +5,6 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-# configure your name and email if you have not done so
-git config --global user.email "fbaffie@gmail.com"
-git config --global user.name "Florian Kobierska Baffie"
-
 # clone the repository to the book-output directory
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/bookdown-demo.git book-output
 cd book-output
