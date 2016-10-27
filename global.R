1ba7db261ec18b7ba78f38ed132a7e445ecd438f
fbaffie <fbaffie@gmail.com>
2016-10-27 15:26:00 +0200

    mixed bymans stuff. it works

--- global.R
+++ global.R
@@ -87,3 +87,61 @@ HBV_2014_diff$Values <- HBV_2014_SimH90$Values - HBV_2014_SimL90$Values
 
 HBV_2014_diff_maxed <- group_by(HBV_2014_diff, nbname, regine.main) %>% dplyr::summarise(maxed = max(Values, na.rm = TRUE))
 stations$uncertainty <- HBV_2014_diff_maxed$maxed[index_HBV] / flom_obs1Y$Values[index_flomtabell[index_HBV]]
+
+
+
+
+
+
+
+
+
+##############################
+#-- byman ####################
+
+
+
+library(reshape2)
+library(ggplot2)
+library(plyr)
+library(shiny)
+library(hydroGOF)
+library(topmodel)
+library(DT)
+library(dygraphs)
+library(zoo)
+library(plotrix)
+library(dplyr)
+library(tidyr)
+library(grid)
+
+# Visualizatin development
+
+
+
+dataDir <- "//nve/fil/h/Fastgrupper/Flomvarsling/FoU/Prosjekt/80103_Hydrologiske_prognoser/arbeidspakker/AP5/DDM/projects/Flood_forecasting/data"
+
+load(file.path(dataDir,"AllStationsAllModels0.RData"))
+#source(file = "//nve/fil/h/Fastgrupper/Flomvarsling/FoU/Prosjekt/80103_Hydrologiske_prognoser/arbeidspakker/AP5/DDM/skript/script/hitrate.function.R")
+
+source(file = file.path(dataDir,"hitrate_alt2.function.R"))
+
+#row.names(stnDatMaster) <- seq(nrow(stnDatMaster)) #removes strange row names from the data frame
+
+#names(stnDatMaster)[11]<-"Catchment"
+
+stationNames <- unique(stnDatMaster$stN)
+modelNames <- unique(stnDatMaster$model)
+
+listForecast <- list.files(path=dataDir,pattern = "_AllStationsForecasts.RData")
+
+load(file.path(dataDir, listForecast[1]))
+names(ddmFdataM)[5 ] <- strsplit(listForecast[1],"_")[[1]][1]
+forecastDat <- ddmFdataM
+
+for (i in 2:length(listForecast)){
+  load(file.path(dataDir, listForecast[i]))
+  forecastDat <- cbind(forecastDat,ddmFdataM[,5])
+  names(forecastDat)[i+4] <- strsplit(listForecast[i],"_")[[1]][1]
+}
+row.names(forecastDat) <- NULL 
--- ui.R
+++ ui.R
@@ -1,7 +1,55 @@
+
+renderInputs <- function(prefix) {
+  
+  wellPanel( 
+    fluidRow( 
+      column(2, 
+             selectInput('catch', 'Catchment to analyse', choices=c("",as.character(unique(stnDatMaster$stN))),selected = "2.323_Fura")),
+      
+      column(1, 
+             checkboxGroupInput('show_vars',
+                                'Model Selection:',
+                                c("HBV",  "DDD", "DDM","ODM"),
+                                selected=c("HBV",  "DDD", "DDM","ODM"))),
+      
+      column(2,
+             
+             checkboxGroupInput('show_meas','Model fitness performance:',
+                                c("model","RMSE","NSE","KGE","BIAS-%"),selected = c("model","RMSE","NSE","KGE","BIAS-%"))
+             #names(evTable)[2:6],selected = names(evTable)[2:6])
+             #                   helpText('For the efficiency measures data, we can select variables
+             #                        to show in the table; for the mtcars example, we
+             #                        use orderClasses = TRUE so that sorted columns 
+             #                        are colored since they have special CSS classes 
+             #                        attached; for the data, we customize the 
+             #                        length menu so we can display 5 rows per page.')
+      ),
+      column(3,
+             h3(textOutput('myText'))),
+      
+      column(2,
+             #h3(textOutput('myText'))
+             dateRangeInput('dateRange',
+                            label = ' Zoom plot: Start Date range input: dd/mm/yy',
+                            start = range(range(stnDatMaster$Date))[1], end = range(range(stnDatMaster$Date))[2]),
+             
+             
+             dateRangeInput('dateRange2',
+                            label = paste('Forecasting data range: ,',
+                                          'dd/mm/yy'),
+                            start = Sys.Date() - 1, end = Sys.Date() +10,
+                            min = Sys.Date() , max = Sys.Date() + 10,
+                            separator = " to ", format = "yyyy-mm-dd"))#,
+      
+      #             column(2,
+      #               h3(textOutput('myText'))
+      #                   )
+    ))
+  
+} 
+
+
 # This is the user-interface definition of a Shiny web application.
-
-# test if shinyserver responds
-
 ui <- navbarPage("Flomvarsling", collapsible = TRUE, theme = "my_style.css",
                  
                  #                  tabPanel("About", icon = icon("info"),
@@ -97,13 +145,64 @@ ui <- navbarPage("Flomvarsling", collapsible = TRUE, theme = "my_style.css",
                                      table_modUI("DDD_table")
                             )
                  ),
+         
+         ##############################
+         #-- byman ####################
+         
                  navbarMenu("Historical tools", icon = icon("history"),
-                            tabPanel("Past forecasting performance"
-                                     #                                      mapModuleUI("past_map"),
-                                     #                                      multimod_forecast_selection_modUI("past_plot"),
-                                     #                                      multimod_forecast_plot_modUI("past_plot")
-                            ),
-                            tabPanel("Past events"),
-                            tabPanel("Calibration results")
-                 )
+                            tabPanel("EntirePlot",dygraphOutput("mydygraph",height = 650)),
+                            tabPanel("Seasonal Plot Temp",plotOutput('annualTemp')),
+                            tabPanel("Seasonal Plot Rain",plotOutput('annualRainfall')),
+                            tabPanel("Seasonal Plot Runoff",plotOutput('annualFlow' ))
+                 ),
+         hr(), 
+         
+         fluidRow( 
+           
+           renderInputs("a")
+         ), 
+         
+         
+         fluidRow( 
+           column(4, 
+                  mainPanel(
+                    tabsetPanel(
+                      tabPanel('Model performnce',
+                               # dygraphOutput("mygraph2",height = 600)),
+                               DT::dataTableOutput("mytable")),
+                      tabPanel('Peaks: Hits/Misses',
+                               DT::dataTableOutput("tablehits"))
+                      # tabPanel('Accum. Diff.',
+                      #          DT::dataTableOutput("mytable11"))
+                      
+                    ))),
+           
+           column(3, 
+                  plotOutput('TDplot', height = "600px")), 
+           
+           column(5,
+                  mainPanel(
+                    tabsetPanel(
+                      tabPanel('Plot - 9 d. ahead',
+                               # dygraphOutput("mygraph2",height = 600)),
+                               plotOutput('mygraph3', height = "600px")),
+                      tabPanel('Table- 9 d. ahead',
+                               DT::dataTableOutput("mytable1")),
+                      tabPanel('Plot - 20 d. past ',
+                               # dygraphOutput("mygraph2",height = 600)),
+                               plotOutput('mygraph2', height = "600px")),
+                      
+                      tabPanel('Month Table',
+                               DT::dataTableOutput("mytable2"))#,
+                      # tabPanel('Plot - 1 year',
+                      #          plotOutput('', height = "600px"))
+                      
+                    ))
+           ) 
+         )
+
+
+         
+         
+         
 )
