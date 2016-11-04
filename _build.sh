07ebe4ae137c7a4ecc7e0967d739e1776e19d454
Florian Kobierska Baffie <fbaffie@gmail.com>
2016-11-04 09:57:30 +0100

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 pwd
+cd report
+pwd
+Rscript -e "rmarkdown::render("process.Rmd")"
+Rscript -e "rmarkdown::render("app.Rmd")"
+Rscript -e "rmarkdown::render("HBV.Rmd")"
+Rscript -e "rmarkdown::render("DDD.Rmd")"
+Rscript -e "rmarkdown::render("ODM.Rmd")"
+Rscript -e "rmarkdown::render("DDM.Rmd")"
 
-Rscript -e "source("render_all_rmd.R")"
+#Rscript -e "source("render_all_rmd.R")"
 
 #Rscript -e "rmarkdown::render("report/process.Rmd")"
 #Rscript -e "rmarkdown::render("report/app.Rmd")"
